d3fc816306e3f3a6b30b7563f7b0bbfedb577837
plznow <108252264+plznowarn@users.noreply.github.com>
2025-04-15 18:56:16 +0900

    Add files via upload

--- FileName.cpp
+++ FileName.cpp
@@ -1,27 +1,20 @@
 #include <stdio.h>
-#include <math.h>
-int main() {
-    int num;
-    scanf("%d", &num);
-    for (int i = 0; i < num; i++) {
-        int x1, y1, r1, x2, y2, r2;
-        scanf("%d", &x1);
-        scanf("%d", &y1);
-        scanf("%d", &r1);
-        scanf("%d", &x2);
-        scanf("%d", &y2);
-        scanf("%d", &r2);
-        int dx = pow(x1 - x2, 2);
-        int dy = pow(y1 - y2, 2);
-        int dr = pow(r1 + r2, 2);
-        if (dx + dy < dr) {
-            printf("2");
-        }
-        else if (dx + dy == dr) {
-            printf("1");
-        }
-        else if (dx + dy > dr) {
-            printf("0");
-        }
-    }
+int main(void) {
+	int num;
+	printf("숫자 입력:");
+	scanf_s("%d", &num);
+	for (int i=0; i < num*2-1; i++) {
+		for (int k = abs(num-i); k*k > 0; k--) {
+			printf("          ");
+		}
+
+		printf("집가고싶다");
+
+		for (int j = 2*i; j >0; j--) {
+			printf("          ");
+		}
+		printf("집가고싶다");
+		printf("\n");
+	}
 }
+
